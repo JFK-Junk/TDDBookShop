539c9c1f5cf4bf809abf4aad248ec95df9dbec3b
Jakob Farian Krarup <kr@rup.dk>
2025-11-03 09:27:54 +0100

    First test passes

--- TDDBookShop/UnitTest1.cs
+++ TDDBookShop/UnitTest1.cs
@@ -8,11 +8,11 @@ public class Tests
     public void WhenAddingABookInAnInvalidStateToTheCollection_TheBookIsRejected()
     {
         //arrange
-        Book book = new("098709812345", "C# for beginners", "Code Jockey Nielsen", 249.95m, 2023);
+        Book book = new("098709812345", "C# for beginners", "Code Jockey Nielsen", -249.95m, 2023);
         BookCollection collection = new();
         
         //act and Assert
         Assert.Throws<ArgumentException>(
-        collection.Add(book));
+        () => collection.Add(book));
     }
 }
